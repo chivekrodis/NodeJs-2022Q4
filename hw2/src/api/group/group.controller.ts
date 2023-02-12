3b6f827103ba9cafa912fc6c32c684e5644f876d
Mikhail Sidarkevich <mikhail_sidarkevich@epam.com>
2023-02-12 21:03:02 +0200

    feat: add error handlers to routs

--- hw2/src/api/group/group.controller.ts
+++ hw2/src/api/group/group.controller.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response, NextFunction } from 'express';
+import { asyncErrorHandler, asyncErrorHandlerWithParams, CreateError } from '../../common';
 import { isUUID } from '../../utils';
 import { IGroup, IGroupRequest } from './group.model';
 import { groupService } from './group.service';
@@ -6,26 +7,30 @@ import { groupSchemaValidation } from './group.validation';
 
 const router = Router();
 
-router.get('/', async (req: Request, res: Response<IGroup[]>) => {
-  const users = await groupService.getAll();
+router.get(
+  '/',
+  asyncErrorHandler(async (req: Request, res: Response<IGroup[]>) => {
+    const groups = await groupService.getAll();
 
-  res.json(users);
-});
+    res.json(groups);
+  }),
+);
 
-router.post('/', groupSchemaValidation.createGroupSchema, async (req: Request, res: Response<IGroup | unknown>) => {
-  try {
+router.post(
+  '/',
+  groupSchemaValidation.createGroupSchema,
+  asyncErrorHandler(async (req: Request, res: Response<IGroup | unknown>) => {
     const newGroup = await groupService.createGroup(req.body);
 
     res.status(201).send(newGroup);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+  }),
+);
 
 router
-  .param('groupId', async (req: IGroupRequest, res: Response, next: NextFunction, id: string) => {
-    try {
-      if (!isUUID(id)) {
+  .param(
+    'groupId',
+    asyncErrorHandlerWithParams(async (req: IGroupRequest, res: Response, next: NextFunction, id: string) => {
+      if (!id || !isUUID(id)) {
         res.send('Param is not UUID');
       }
 
@@ -34,14 +39,12 @@ router
       if (group) {
         req.group = group;
       } else {
-        return res.status(404).send('Not found');
+        throw new CreateError(404, 'Not found');
       }
 
       return next();
-    } catch (error) {
-      res.send(error);
-    }
-  })
+    }),
+  )
   .get('/:groupId', async (req: IGroupRequest, res: Response<IGroup>) => {
     const { group } = req;
 
@@ -50,30 +53,29 @@ router
   .put(
     '/:groupId',
     groupSchemaValidation.updateGroupSchema,
-    async (req: IGroupRequest, res: Response<IGroup | unknown>) => {
-      try {
-        const {
-          group,
-          params: { groupId },
-        } = req;
+    asyncErrorHandler(async (req: IGroupRequest, res: Response<IGroup | unknown>) => {
+      const {
+        group,
+        params: { groupId },
+      } = req;
 
-        if (group) {
-          const updatedData = await groupService.updateGroup(groupId, group?.toJSON(), req.body);
+      if (group) {
+        const updatedData = await groupService.updateGroup(groupId, group?.toJSON(), req.body);
 
-          res.send(updatedData);
-        }
-      } catch (error) {
-        res.status(400).send(error);
+        res.send(updatedData);
       }
-    },
+    }),
   )
-  .delete('/:groupId', async (req: IGroupRequest, res: Response<string>) => {
-    const { groupId } = req.params;
-    const deletedGroupId = await groupService.deleteGroup(groupId);
+  .delete(
+    '/:groupId',
+    asyncErrorHandler(async (req: IGroupRequest, res: Response<string>) => {
+      const { groupId } = req.params;
+      const deletedGroupId = await groupService.deleteGroup(groupId);
 
-    if (deletedGroupId) {
-      res.send(deletedGroupId);
-    }
-  });
+      if (deletedGroupId) {
+        res.send(deletedGroupId);
+      }
+    }),
+  );
 
 export const groupRouter = router;
--- hw2/src/app.ts
+++ hw2/src/app.ts
@@ -1,14 +1,40 @@
 import express, { Express, Response } from 'express';
 import api from './api';
+import { logger, reqLogger, errorHandler } from './common';
 
 const app: Express = express();
 
 app.use(express.json());
+
+app.use(reqLogger);
 
 app.get('/', (_, res: Response<string>) => {
   res.json('Hello World');
 });
 
 app.use('/api/v1', api);
+
+app.use(errorHandler);
+
+process.on('uncaughtException', (error, origin) => {
+  logger.log({
+    timestamp: new Date().toISOString(),
+    level: 'error',
+    error,
+    message: origin,
+    stack: error.stack,
+  });
+});
+
+process.on('unhandledRejection', (reason: Error, promise) => {
+  logger.log({
+    timestamp: new Date().toISOString(),
+    level: 'error',
+    message: 'unhandledRejection',
+    reason: reason.message,
+    promise,
+    stack: reason.stack,
+  });
+});
 
 export default app;
